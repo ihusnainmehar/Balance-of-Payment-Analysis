941680476d284281e2b4e86bf1d35a6bc111c63c
Husnain Mehar <shahzadahusnain.sh@gmail.com>
2024-12-15 17:28:54 +0300

    Checking for Duplicates i.e. Data Cleaning

---
+++ Balance of Payment Script.sql
@@ -0,0 +1,34 @@
+/* Balance of Payment Analysis for five (5) European Countries
+Created by Group 1, InternPulse
+December 9th, 2024*/
+
+-- Checking for Duplicates (Albania)
+
+SELECT [Year], COUNT(*) AS occurrences
+FROM [dbo].[Albania]
+GROUP BY [Year]
+HAVING COUNT(*) > 1;
+
+-- Checking for Duplicates (Austria)
+SELECT [Year], COUNT(*) AS occurrences
+FROM [dbo].[Austria]
+GROUP BY [Year]
+HAVING COUNT(*) > 1;
+
+-- Checking for Duplicates (Belgium)
+SELECT [Year], COUNT(*) AS occurrences
+FROM [dbo].[Belgium]
+GROUP BY [Year]
+HAVING COUNT(*) > 1;
+
+-- Checking for Duplicates (France)
+SELECT [Year], COUNT(*) AS occurrences
+FROM [dbo].[France]
+GROUP BY [Year]
+HAVING COUNT(*) > 1;
+
+-- Checking for Duplicates (Germany)
+SELECT [Year], COUNT(*) AS occurrences
+FROM [dbo].[Germany]
+GROUP BY [Year]
+HAVING COUNT(*) > 1;
